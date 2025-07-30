411c8a2836294dfb0f9dcb9ade006eb7cb23ed46
Justin Rhoads <rhoadsjustin@gmail.com>
2025-07-29 22:20:21 -0500

    initial commit

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from 'react-native';
+import { Button } from './Button';
 
 export const EditScreenInfo = ({ path }: { path: string }) => {
   const title = 'Open up the code for this screen:';
@@ -10,9 +11,14 @@ export const EditScreenInfo = ({ path }: { path: string }) => {
       <View className={styles.getStartedContainer}>
         <Text className={styles.getStartedText}>{title}</Text>
         <View className={styles.codeHighlightContainer + styles.homeScreenFilename}>
-          <Text>{path}</Text>
+          <Text>Justin</Text>
         </View>
         <Text className={styles.getStartedText}>{description}</Text>
+        <Button
+          className='bg-yellow-500'
+          title="Edit this screen"
+          onPress={() => console.warn('Edit this screen pressed')}
+        />
       </View>
     </View>
   );
@@ -20,7 +26,7 @@ export const EditScreenInfo = ({ path }: { path: string }) => {
 
 const styles = {
   codeHighlightContainer: `rounded-md px-1`,
-  getStartedContainer: `items-center mx-12`,
+  getStartedContainer: `items-center mx-6`,
   getStartedText: `text-lg leading-6 text-center`,
   helpContainer: `items-center mx-5 mt-4`,
   helpLink: `py-4`,
